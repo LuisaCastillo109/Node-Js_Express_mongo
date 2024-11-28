5beeadd9fa2a33e2635643b61151bf0b7d039918
Luisa Castillo <163435574+LuisaCastillo109@users.noreply.github.com>
2024-11-27 22:23:39 -0500

    Agregando y probando la funcionalidad para editar cursos.Endpoint de tipo PUT

--- controllers/curso.js
+++ controllers/curso.js
@@ -7,6 +7,15 @@ ruta.get('/', (req,res)=>{
 res.json('Respuesta a peticion GET de cursos funcionando correctamente');   
  });
 
+ruta.put('/:id',(req,res) =>{
+let resultado =actualizarUsuario(req.params.id, req.body);
+resultado.then(curso =>{
+    res.json(curso)
+}).catch(err => {
+    res.status(400).json(err)
+})
+})
+
 
  ruta.post('/',(req,res) => {
     let resultado =crearCurso(req.body);
@@ -29,5 +38,15 @@ res.json('Respuesta a peticion GET de cursos funcionando correctamente');
         calificacion: body.calificacion
     });
     return await curso.save();
+ }
+
+ async function actualizarUsuario(id,body){
+    let curso = await Curso.findByIdAndUpdate(id,{
+        $set:{
+            titulo:body.titulo,
+            descripcion:body.descripcion
+        }
+    },{new: true});
+    return curso;
  }
 module.exports=ruta;
