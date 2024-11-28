f69c872ea796a4a7db705bb46a8400376f4530d9
Luisa Castillo <163435574+LuisaCastillo109@users.noreply.github.com>
2024-11-27 21:37:44 -0500

    Agregando el modelo curso

--- controllers/curso.js
+++ controllers/curso.js
@@ -7,4 +7,27 @@ ruta.get('/', (req,res)=>{
 res.json('Respuesta a peticion GET de cursos funcionando correctamente');   
  });
 
+
+ ruta.post('/',(req,res) => {
+    let resultado =crearCurso(req.body);
+
+    resultado.then(curso => {
+        res.json({
+            curso
+        })
+    }).catch(err => {
+        res.status(400).json({
+          err
+        })
+    })
+ });
+ async function crearCurso(body){
+    let curso = new Curso({
+        titulo  :body.titulo,
+        descipcion :body.descipcion,
+        alumnos :body.alumnos,
+        calificacion: body.calificacion
+    });
+    return await curso.save();
+ }
 module.exports=ruta;
